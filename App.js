3ebd4f98f6c67c2b9b2391767f30334988128564
Teemu Puurunen <teemu.puurunen@finago.com>
2018-11-13 13:33:20 +0200

    Session 1. Basic state management.

--- App.js
+++ App.js
@@ -1,11 +1,67 @@
 import React, { Component } from "react";
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newItemValue: "New item",
+      items: ["Hammer", "Nails", "Stuff"]
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.addItem = this.addItem.bind(this);
+  }
+
+  addItem = () => {
+    this.setState({
+      items: [...this.state.items, this.state.newItemValue]
+    });
+  };
+
+  handleChange(event) {
+    this.setState({ newItemValue: event.target.value });
+  }
+
   render() {
     return (
       <div>
-        <h1>Hello World</h1>
+        <h1>Storage Manager</h1>
+        <StorageList list={this.state.items} />
+        <NewItemForm
+          addNewItemAction={this.addItem}
+          newItemValue={this.state.newItemValue}
+          handleChange={this.handleChange}
+        />
       </div>
     );
   }
 }
+
+const StorageList = ({ list }) => (
+  <ul>
+    {list.map(item => (
+      <Item key={item} item={item} />
+    ))}
+  </ul>
+);
+
+const NewItemForm = ({ addNewItemAction, newItemValue, handleChange }) => (
+  <form>
+    <input
+      type="text"
+      name="newItem"
+      value={newItemValue}
+      onChange={handleChange}
+    />
+    <AddItemButton buttonAction={addNewItemAction} />
+  </form>
+);
+
+const AddItemButton = ({ buttonAction }) => (
+  <button type="button" onClick={() => buttonAction()}>
+    Add item
+  </button>
+);
+
+const Item = ({ item }) => <li>{item}</li>;
+
 export default App;
