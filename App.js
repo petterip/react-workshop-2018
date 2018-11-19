ad844f22f220001fe61b046d926a29edf96776c5
Teemu Puurunen <teemu.puurunen@finago.com>
2018-11-19 15:26:05 +0200

    Removing items from storage list.

--- App.js
+++ App.js
@@ -1,26 +1,39 @@
 import React, { Component } from "react";
+import _ from "lodash";
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       newItemValue: "New item",
-      items: {
-        "item-1": "Hammer",
-        "item-2": "Nails",
-        "item-3": "Stuff"
-      }
+      items: [
+        { id: "item-1", name: "Hammer" },
+        { id: "item-2", name: "Drill" },
+        { id: "item-3", name: "Nails 'n' stuff " }
+      ]
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.addItem = this.addItem.bind(this);
+    this.deleteItemAction = this.deleteItemAction.bind(this);
   }
 
   addItem() {
     let timestamp = new Date().getTime();
     let key = "item-" + timestamp;
+    let newItem = { id: "item-" + timestamp, name: this.state.newItemValue };
 
     this.setState({
-      items: { ...this.state.items, [key]: this.state.newItemValue }
+      items: [...this.state.items, newItem]
+    });
+  }
+
+  deleteItemAction(key) {
+    const filteredItems = _.filter(this.state.items, item => {
+      return item.id !== key;
+    });
+
+    this.setState({
+      items: filteredItems
     });
   }
 
@@ -32,7 +45,10 @@ class App extends Component {
     return (
       <div>
         <h1>Storage Manager</h1>
-        <StorageList list={this.state.items} />
+        <StorageList
+          list={this.state.items}
+          deleteItemAction={this.deleteItemAction}
+        />
         <NewItemForm
           addNewItemAction={this.addItem}
           newItemValue={this.state.newItemValue}
@@ -43,10 +59,10 @@ class App extends Component {
   }
 }
 
-const StorageList = ({ list }) => (
+const StorageList = ({ list, deleteItemAction }) => (
   <ul>
-    {Object.keys(list).map(key => (
-      <Item key={key} item={list[key]} />
+    {list.map(item => (
+      <Item key={item.id} deleteItemAction={deleteItemAction} item={item} />
     ))}
   </ul>
 );
@@ -69,6 +85,11 @@ const AddItemButton = ({ buttonAction }) => (
   </button>
 );
 
-const Item = ({ item }) => <li>{item}</li>;
+const Item = ({ item, deleteItemAction }) => (
+  <li>
+    {item.name} -{" "}
+    <button onClick={() => deleteItemAction(item.id)}>Delete</button>
+  </li>
+);
 
 export default App;
